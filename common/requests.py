9f7417f89ab802e080ba2950d5538c6c65fd45f9
Cole Dylewski <cole.dylewski@gmail.com>
2024-09-05 13:36:48 -0400

    added inter docker api call

--- common/requests.py
+++ common/requests.py
@@ -10,3 +10,15 @@ def internet_test():
         # print("No internet connection...")
         time.sleep(5)
         return False
+
+def fastAPI():
+    url = "http://nginx/api/v2"
+    timeout = 5
+    try:
+        request = requests.get(url, timeout=timeout)
+        print(request.text)
+        return True
+    except (requests.ConnectionError, requests.Timeout) as exception:
+        # print("No internet connection...")
+        time.sleep(5)
+        return False
